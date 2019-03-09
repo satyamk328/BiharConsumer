7cc74707aa8cf3255147d83f42e07752f52d941b
satyam.kumar <satyam.kumar@GDC-SatyamK-LPT.agreeya.local>
2019-03-09 22:35:06 +0530

    SaDS

--- src/com/digital/config/WebSecurityConfig.java
+++ src/com/digital/config/WebSecurityConfig.java
@@ -6,7 +6,6 @@ package com.digital.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -31,23 +30,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("bill").password("abc123").roles("ADMIN");
+		auth.inMemoryAuthentication().withUser(username).password(password).authorities("ROLE_USER");
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		 http.authorizeRequests().antMatchers("/api/v0/build-info").permitAll();
-		 http.authorizeRequests().antMatchers("/v2/api-docs", "/swagger-ui.html",
-		"/webjars/**", "/swagger-resources/**")
-		 .denyAll();
-
-		http.csrf().disable().authorizeRequests().anyRequest().denyAll()
-				.and().httpBasic().authenticationEntryPoint(authenticationEntryPoint).and().sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.authorizeRequests().antMatchers("/api/v0/**").permitAll();
+		http.authorizeRequests().antMatchers("/v2/api-docs", "/swagger-ui.html", "/webjars/**", "/swagger-resources/**")
+				.permitAll();
+		http.authorizeRequests().antMatchers("/images/**","/configuration/**").permitAll();
+		http.headers().frameOptions().deny();
+		http.authorizeRequests().anyRequest().authenticated();
+		http.httpBasic().authenticationEntryPoint(authenticationEntryPoint);
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		http.exceptionHandling().authenticationEntryPoint(authenticationEntryPoint);
+		http.csrf().disable();
 	}
 
-	/* To allow Pre-flight [OPTIONS] request from browser */
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
