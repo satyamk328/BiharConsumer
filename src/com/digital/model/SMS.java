859541f77e4fccdb8f05f2f801163bec0483fcfd
satyamk328 <satyamk328@satyamk328@gmail.com>
2019-03-09 12:57:44 +0530

    sdf

---
+++ src/com/digital/model/SMS.java
@@ -0,0 +1,53 @@
+package com.digital.model;
+
+public class SMS {
+	private String name;
+	private String userName;
+	private String password;
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+
+	/**
+	 * @param userName
+	 *            the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password
+	 *            the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
--- src/com/digital/service/SMSWrapperService.java
+++ src/com/digital/service/SMSWrapperService.java
@@ -1,6 +1,7 @@
 package com.digital.service;
 
 import java.nio.charset.Charset;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,12 +13,14 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Base64Utils;
 
 import com.digital.audit.service.DBLoggingHandler;
 import com.digital.audit.service.RestTemplateService;
+import com.digital.model.SMS;
 import com.digital.model.SMSResponse;
 
 @Service
@@ -33,22 +36,36 @@ public class SMSWrapperService {
 
 	@Value("${sms_gateWay_URL}")
 	private String smsGateWayAPi;
-	
+
 	@Value("${sms_tocken}")
 	private String smsAccessToken;
 
-	public SMSResponse sendSMS(String phoneNumber) {
+	public SMSResponse sendSMSGET(String phoneNumber) {
 		log.debug("smsGateWayAPi ={}", smsGateWayAPi);
+
+		Map<String, Object> param = new HashMap<String, Object>();
+		param.put("transactionId", 1);
+
+		HttpHeaders httpHeaders = new HttpHeaders();
+		// set up HTTP Basic Authentication Header
+		httpHeaders.add("X-Auth-Token", smsAccessToken);
+		String username = "test", password = "123";
+		String auth = username + ":" + password;
+		byte[] encodedAuth = Base64Utils.encode(auth.getBytes(Charset.forName("US-ASCII")));
+		String authHeader = "Basic " + new String(encodedAuth);
+		httpHeaders.set("Authorization", authHeader);
+		httpHeaders.add("Accept", MediaType.APPLICATION_JSON_VALUE);
+
+		// request entity is created with request headers
+		HttpEntity<SMSResponse> requestEntity = new HttpEntity<>(httpHeaders);
 		ResponseEntity<SMSResponse> response = null;
 		try {
-			Map<String, Object> uriVariables = new HashMap<String, Object>();
-			uriVariables.put("transactionId", 1);
-			response = restTemplateService.getRestTemplate().exchange(smsGateWayAPi, HttpMethod.GET,
-					new HttpEntity<SMSResponse>(createXAuthHeader(smsAccessToken)), SMSResponse.class, uriVariables);
+			response = restTemplateService.getRestTemplate().exchange(smsGateWayAPi, HttpMethod.GET, requestEntity,
+					SMSResponse.class, param);
 			if (response != null && response.getStatusCode() == HttpStatus.OK) {
 				SMSResponse cardVaultResponse = response.getBody();
 				return cardVaultResponse;
-			} else {	
+			} else {
 				log.error("Failed to fetch card record for PhoneNumber={} , Http Status={}", phoneNumber,
 						response != null ? response.getStatusCode() : "ResponseIsNull");
 				dbLoggingHandler.logError("Failed to fetch card record for PhoneNumber=" + phoneNumber
@@ -63,18 +80,52 @@ public class SMSWrapperService {
 		return null;
 	}
 
-	private HttpHeaders createXAuthHeader(String xAuthHeader) {
-		log.info("Creating XAuthHeader , xAuthHeader={}", xAuthHeader);
+	public SMSResponse sendSMSPost(String phoneNumber) {
+		log.debug("smsGateWayAPi ={}", smsGateWayAPi);
+
+		String username = "test", password = "123";
+		
+		Map<String, Object> param = new HashMap<String, Object>();
+		param.put("transactionId", 1);
+
 		HttpHeaders httpHeaders = new HttpHeaders();
-		httpHeaders.add("X-Auth-Token", xAuthHeader);
-		
-		String username = "test",password ="123";
-		String auth = username + ":" + password;  
-		byte[] encodedAuth = Base64Utils.encode(auth.getBytes(Charset.forName("US-ASCII")) );
-        String authHeader = "Basic " + new String( encodedAuth );
+		httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+		httpHeaders.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		// set up HTTP Basic Authentication Header
+		String auth = username + ":" + password;
+		byte[] encodedAuth = Base64Utils.encode(auth.getBytes(Charset.forName("US-ASCII")));
+		String authHeader = "Basic " + new String(encodedAuth);
 		httpHeaders.set("Authorization", authHeader);
-		
-		return httpHeaders;
+
+		//setting up the request body
+        SMS user = new SMS();
+        user.setName("Sample User");
+        user.setUserName("user1");
+        user.setPassword("pass123");
+        
+		// request entity is created with request headers
+        HttpEntity<SMS> requestEntity = new HttpEntity<>(user, httpHeaders);
+
+		ResponseEntity<SMSResponse> response = null;
+		try {
+			response = restTemplateService.getRestTemplate().exchange(smsGateWayAPi, HttpMethod.POST, requestEntity,
+					SMSResponse.class);
+			if (response != null && response.getStatusCode() == HttpStatus.OK) {
+				SMSResponse cardVaultResponse = response.getBody();
+				return cardVaultResponse;
+			} else {
+				log.error("Failed to fetch card record for PhoneNumber={} , Http Status={}", phoneNumber,
+						response != null ? response.getStatusCode() : "ResponseIsNull");
+				dbLoggingHandler.logError("Failed to fetch card record for PhoneNumber=" + phoneNumber
+						+ ", Http Status=" + (response != null ? response.getStatusCode() : "ResponseIsNull"));
+			}
+		} catch (Exception ex) {
+			log.error("Failed to get response while fetch card vault token for PhoneNumber={}, Exp Msg={}", phoneNumber,
+					ex.getMessage(), ex);
+			dbLoggingHandler.logError("Failed to get response while fetch card vault token for PhoneNumber="
+					+ phoneNumber + ", Exp Msg=" + ex.getMessage());
+		}
+		return null;
 	}
 
 }
