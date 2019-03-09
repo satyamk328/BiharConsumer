fbee155c1bc6f8761617daf3f8afb4bcf458cc70
satyam.kumar <satyam.kumar@GDC-SatyamK-LPT.agreeya.local>
2019-03-09 16:05:41 +0530

    asdasd

--- src/com/digital/config/WebSecurityConfig.java
+++ src/com/digital/config/WebSecurityConfig.java
@@ -6,9 +6,12 @@ package com.digital.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -26,22 +29,27 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private HttpAuthenticationEntryPoint authenticationEntryPoint;
 
-	@Override
-	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("javainuse").password("javainuse").roles("USER");
+	@Autowired
+	public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("bill").password("abc123").roles("ADMIN");
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// http.authorizeRequests().antMatchers("/api/v0/build-info").permitAll();
-		// http.authorizeRequests().antMatchers("/v2/api-docs", "/swagger-ui.html",
-		// "/webjars/**", "/swagger-resources/**")
-		// .permitAll();
+		 http.authorizeRequests().antMatchers("/api/v0/build-info").permitAll();
+		 http.authorizeRequests().antMatchers("/v2/api-docs", "/swagger-ui.html",
+		"/webjars/**", "/swagger-resources/**")
+		 .denyAll();
 
-		http.authorizeRequests().anyRequest().authenticated();
-		http.httpBasic().authenticationEntryPoint(authenticationEntryPoint);
-		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.csrf().disable().authorizeRequests().anyRequest().denyAll()
+				.and().httpBasic().authenticationEntryPoint(authenticationEntryPoint).and().sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		http.exceptionHandling().authenticationEntryPoint(authenticationEntryPoint);
-		http.csrf().disable();
+	}
+
+	/* To allow Pre-flight [OPTIONS] request from browser */
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
 	}
 }
