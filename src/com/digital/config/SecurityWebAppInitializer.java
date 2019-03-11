70af1a7ba3eb8cb641835ed6b8655db11aadfdde
satyam.kumar <satyam.kumar@GDC-SatyamK-LPT.agreeya.local>
2019-03-11 11:24:00 +0530

    a

--- src/com/digital/config/SecurityWebAppInitializer.java
+++ src/com/digital/config/SecurityWebAppInitializer.java
@@ -2,6 +2,6 @@ package com.digital.config;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
-public class SecurityWebAppInitializer extends AbstractSecurityWebApplicationInitializer {
+public class SecurityWebAppInitializer {//extends AbstractSecurityWebApplicationInitializer {
 
 }
