57754a6b15f62ff182e6a2c132287667d4b97d07
Satyam Kumar <satyam.kumar@agreeya.net>
2019-03-13 10:16:46 +0530

    Commit

---
+++ src/com/digital/dao/BookingDao.java
@@ -0,0 +1,11 @@
+package com.digital.dao;
+
+import org.springframework.stereotype.Repository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Repository("bookingDao")
+@Slf4j
+public class BookingDao {
+
+}
